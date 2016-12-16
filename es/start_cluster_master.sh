e2ad5957b8daf571712bd980ae1a5c812dd17ea9
Carlos Gonzalez <carlosj.gf@gmail.com>
2016-12-16 16:57:51 +0100

    ES cluster

---
+++ es/start_cluster_master.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+NAME=elastic_master
+IMAGE=netcomposer/elasticsearch:5.0.2-3
+
+docker rm $NAME > /dev/null 2>&1
+
+docker create --rm \
+	--name $NAME \
+	--network elastic \
+	--hostname $NAME \
+	-v elastic_master_data:/usr/share/elasticsearch/data \
+	--env "ES_JAVA_OPTS=-Xms1g -Xmx1g" \
+	$IMAGE \
+		-Enode.name="$NAME" \
+		-Etransport.host="_eth0_" \
+		-Ehttp.host="_eth2_" \
+		-Ecluster.name="NetComposer" \
+		-Ediscovery.zen.ping.unicast.hosts=127.0.0.1:9300
+
+docker network connect nginx $NAME
+
+docker start -a $NAME
+
---
+++ es/start_cluster_slave.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+NAME=elastic_${HOSTNAME//\./_}
+IMAGE=netcomposer/elasticsearch:5.0.2-3
+
+docker rm $NAME > /dev/null 2>&1
+
+docker create --rm \
+	--name $NAME \
+	--network elastic \
+	--hostname $NAME \
+	-v elastic_slave_data:/usr/share/elasticsearch/data \
+	--env "ES_JAVA_OPTS=-Xms1g -Xmx1g" \
+	$IMAGE \
+		-Enode.name="$NAME" \
+		-Ecluster.name="NetComposer" \
+		-Etransport.host="_eth0_" \
+		-Ehttp.host="_eth2_" \
+		-Ediscovery.zen.ping.unicast.hosts=elastic_master:9300
+
+docker network connect nginx $NAME
+
+docker start -a $NAME
