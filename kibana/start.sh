b18d0ddc833145beceb26ec768645980014d1242
Carlos Gonzalez <carlosj.gf@gmail.com>
2016-12-17 01:42:15 +0100

    proxy!

--- kibana/start.sh
+++ kibana/start.sh
@@ -3,8 +3,10 @@ NAME=kibana
 IMAGE=netcomposer/kibana:5.0.2-1
 
 docker rm $NAME > /dev/null 2>&1 
-docker run --rm --name $NAME --net=host \
-	-e "ELASTICSEARCH_URL=http://127.0.0.1:9200" \
+docker run -ti --rm --name $NAME --net=host \
+	-e "ELASTICSEARCH_URL=http://k2.netc.io:80/es/" \
+	-e "ELASTICSEARCH_USERNAME=test" \
+	-e "ELASTICSEARCH_PASSWORD=es" \
 	-e "SERVER_PORT=5601" \
 	$IMAGE 
 
--- kibana/start_service.sh
+++ kibana/start_service.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 IMAGE=netcomposer/kibana:5.0.2-1
+ES="http://elastic_openresty:9200"
 
-docker service create --name kibana --network elastic -p 5601:5601 -e "ELASTICSEARCH_URL=http://elastic_master:9200" -e "SERVER_PORT=5601" $IMAGE 
+docker service create \
+	--name kibana \
+	--network elastic \
+	-p 5601:5601 \
+	--env "ELASTICSEARCH_URL=$ES" \
+	--env "SERVER_PORT=5601" \
+	$IMAGE 
 
 	
